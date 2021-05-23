9b7ee147f1fb27259825621e0d66545198d91b7e
kjablono <persivalus@gmail.com>
2021-05-24 01:47:11 +0200

    restored

--- zadanie.m
+++ zadanie.m
@@ -5,5 +5,5 @@ x0 = [-1, 0];
 global save;
 global comp_with_ode45;
 save = true;
-comp_with_ode45 = false;
-[t, x] = prezentacja(@PK4adams, @func, a, h, x0);
+comp_with_ode45 = true;
+[t, x] = prezentacja(@RK4klasyczna, @func, a, h, x0);
